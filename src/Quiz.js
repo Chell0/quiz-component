98c8d1cb6a97de02bd152098da854fcbd33326ea
Machel Odhiambo <machelgabriel@gmail.com>
2023-09-14 21:28:59 +0300

    Add a 'render()' method in the 'Quiz' class that returns a single '<div>'

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,6 +2,10 @@ import { Component } from "react";
 
 let quizData = require("./quiz_data.json");
 
-class Quiz extends Component {}
+class Quiz extends Component {
+	render() {
+		return <div>Quiz</div>;
+	}
+}
 
 export default Quiz;
