dda0dec9c9a3a107446732f2c12b5f8e723f300b
Machel Odhiambo <machelgabriel@gmail.com>
2023-09-14 21:44:12 +0300

    Set the initial state

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,6 +5,9 @@ let quizData = require("./quiz_data.json");
 class Quiz extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			question: 1,
+		};
 	}
 	render() {
 		return <div>Quiz</div>;
