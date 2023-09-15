bb888187e6f842d61ef7a602d9bc6b54eb6d0774
Machel Odhiambo <machelgabriel@gmail.com>
2023-09-15 10:46:30 +0300

    Add instruction text

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,10 @@ class QuizQuestion extends Component {
 		return (
 			<main>
 				<section>
-					<p>{/* instruction text goes here */}</p>
+					<p>
+						{/* instruction text goes here */}
+						{this.props.quiz_question.instruction_text}
+					</p>
 				</section>
 				<section className="buttons">
 					<ul>
