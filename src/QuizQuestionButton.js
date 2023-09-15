7972bc856bf30b191e925cc023384cd7075f7845
Machel Odhiambo <machelgabriel@gmail.com>
2023-09-15 11:22:17 +0300

    In the 'render()' function create a single 'li' that has a 'button' as a child

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,6 +2,13 @@ import { Component } from "react";
 
 class QuizQuestionButtion extends Component {
 	// Pass
+	render() {
+		return (
+			<li>
+				<button></button>
+			</li>
+		);
+	}
 }
 
 export default QuizQuestionButtion;
